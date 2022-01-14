69b72f45dcbb0b9b30c052c5a905311da787d1c3
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2022-01-14 11:21:50 +0100

    files added

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Popper from '@popperjs/core';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
+
